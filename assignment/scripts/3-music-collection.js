380ef3bacec27d97df213624fe8345407ee8e5a8
Seth Woodson <sethwoodson3@gmail.com>
2023-02-07 11:18:58 -0600

    edited collections

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,19 +10,19 @@ function addToCollection(title, artist, yearPublished) {
     collection.push(album);
     return album;
 };
-addToCollection('good day', 'days go bye', 'up again');
-addToCollection('let you down', 'the search', 'lie');
-addToCollection('if you want love', 'you broke me first', 'if ever');
-addToCollection('mockingbird', 'without me', 'superman');
-addToCollection('spinnin', 'violet', 'splat');
-addToCollection('yeah!', 'my boo', 'omg');
+addToCollection('good day', 'mike.', '2022');
+addToCollection('these days', 'mike.', '2015');
+addToCollection('if you want love', 'nf', '2017');
+addToCollection('mockingbird', 'eminem', '2004');
+addToCollection('spinnin', 'connor price', '2022');
+addToCollection('yeah!', 'usher', '2004');
 
-console.log(addToCollection('good day', 'days go bye', 'up again'));
-console.log(addToCollection('let you down', 'the search', 'lie'));
-console.log(addToCollection('if you want love', 'you broke me first', 'if ever'));
-console.log(addToCollection('mockingbird', 'without me', 'superman'));
-console.log(addToCollection('spinnin', 'violet', 'splat'));
-console.log(addToCollection('yeah!', 'my boo', 'omg'));
+console.log(addToCollection('good day', 'mike.', '2022'));
+console.log(addToCollection('let you down', 'mike.', '2015'));
+console.log(addToCollection('if you want love', 'nf', '2017'));
+console.log(addToCollection('mockingbird', 'eminem', '2004'));
+console.log(addToCollection('spinnin', 'connor price', '2022'));
+console.log(addToCollection('yeah!', 'usher', '2004'));
 
 function showCollection(array) {
     console.log(array.length);
